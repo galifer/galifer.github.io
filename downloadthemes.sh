9cf6b758cb23dc2f6e705a7187a5e464952c3d58
galifer <1159727000@qq.com>
2017-06-07 08:58:50 +0800

    update

--- downloadthemes.sh
+++
@@ -1,2 +0,0 @@
-git clone https://github.com/iissnan/hexo-theme-next.git themes/next
-git clone https://github.com/litten/hexo-theme-yilia.git themes/yilie
---
+++ dthemesandsuser.sh
@@ -0,0 +1,9 @@
+git config user.name "galifer"
+git config user.email "1159727000@qq.com"
+git clone https://github.com/iissnan/hexo-theme-next.git themes/next
+git clone https://github.com/litten/hexo-theme-yilia.git themes/yilie
+hexo g
+hexo d
+cd .deploy_git
+git config user.name "galifer"
+git config user.email "1159727000@qq.com"
